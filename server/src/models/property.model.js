e41334130477f46ca97f2b440f4b6ec49f85c525
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2026-05-19 05:29:07 +0530

    feat: add property model with schema

---
+++ server/src/models/property.model.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+
+const propertySchema = new mongoose.Schema(
+   {
+      title: {
+         type: String,
+         required: true
+      },
+
+      description: {
+         type: String,
+         required: true
+      },
+
+      location: {
+         type: String,
+         required: true
+      },
+
+      city: {
+         type: String,
+         required: true
+      },
+
+      rent: {
+         type: Number,
+         min: 0,
+         required: true
+      },
+
+      bedrooms: {
+         type: Number,
+         min: 0,
+      },
+      bathrooms: {
+         type: Number,
+         min: 0,
+      },
+
+      furnished: {
+         type: Boolean,
+         default: false
+      },
+
+      amenities: [String],
+
+      images: [String],
+
+      owner: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "user",
+         required: true
+      },
+
+      coordinates: {
+         latitude: Number,
+         longitude: Number
+      },
+
+      status: {
+         type: String,
+         enum: ["available", "booked"],
+         default: "available"
+      }
+
+   },
+   {
+      timestamps: true
+   }
+);
+
+const propertyModel = mongoose.model("Property", propertySchema);
+
+export default propertyModel;
