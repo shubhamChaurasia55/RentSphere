ecdbd86575548de6b52ea0c4539fb404ebe4b93c
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2026-05-20 03:24:29 +0530

    feat: add Cloudinary and Multer setup for image uploads

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -14,10 +14,25 @@ if(!process.env.PORT){
     throw new Error("PORT not found");
 }
 
+if(!process.env.CLOUDINARY_CLOUD_NAME){
+    throw new Error("CLOUDINARY_CLOUD_NAME not found");
+}
+
+if(!process.env.CLOUDINARY_API_KEY){
+    throw new Error("CLOUDINARY_API_KEY not found");
+}
+
+if(!process.env.CLOUDINARY_API_SECRET){
+    throw new Error("CLOUDINARY_API_SECRET not found");
+}
+
 const config = {
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
 }
 
 export default config;
