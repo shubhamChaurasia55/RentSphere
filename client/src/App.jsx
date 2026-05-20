fd02d66151440d1e4c4be7e99a0586f17899563a
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2026-05-21 03:19:08 +0530

    feat: setup Vite + React + Tailwind CSS v4 frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './assets/logo.png'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <img src={logo} alt="logo" />
+      </div>
+      <h1 className="text-blue-500 text-4xl">RentSphere</h1>
+    </div>
+  )
+}
+
+export default App
