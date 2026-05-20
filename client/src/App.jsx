76fabf687392a6debefed3410e389fb81d3bc54b
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2026-05-21 04:17:45 +0530

    feat: add React Router with pages (Home, Login, Register, NotFound)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,9 @@
-import logo from './assets/logo.png'
+import AppRoutes from "./routes/AppRoutes";
 
-function App() {
-  return (
-    <div>
-      <div>
-        <img src={logo} alt="logo" />
-      </div>
-      <h1 className="text-blue-500 text-4xl">RentSphere</h1>
-    </div>
-  )
-}
+const App = () => {
 
-export default App
+    return <AppRoutes />;
+
+};
+
+export default App;
